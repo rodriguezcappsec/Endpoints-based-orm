39008a42b9d60e3ad4203faa5fbfc6868ae54e06
luis <luis.rodriguezcastro31@gmail.com>
2019-06-16 14:28:01 -0700

    Testing the orm librarie

---
+++ app/services/endpoint_based_orm.js
@@ -0,0 +1,33 @@
+const getFiles = require("./directoriesRecursion.js");
+var fs = require("fs");
+
+let configs = getFiles("./app/services/endPoints");
+let endPoints = configs.map(config =>
+  eval(`(${fs.readFileSync("./app/services/endPoints/" + "/" + config)})`)
+);
+let evalFunctions = [];
+endPoints.forEach(element => {
+
+  const host = require("./config.json").host;
+  for (const key in element) {
+    
+    evalFunctions.push(`
+      function ${key}(){
+        const http = require("http");
+        const https = require("https");
+        http.${element[key].type}("${host}${element[key].url}", (resp) => {
+            let data = "";
+            resp.on('data', (chunk) => {
+                data += chunk;
+                console.log(data)
+            });
+        })
+      }
+      `);
+  }
+});
+
+evalFunctions.forEach(functions => {
+  eval(functions);
+  all()
+});
