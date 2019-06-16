bd2bfa159b8a63814bf6195d3565a9a1230d7e75
luis <luis.rodriguezcastro31@gmail.com>
2019-06-16 14:28:56 -0700

    Modify routes

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express();
 const { User } = require("../models/user.js");
-var config = require('../services/endPoints.json').users
 
-router.post(config.post_user, async (req, res) => {
-    await User.create({
-        firstName: 'John',
-        lastName: "doeeee",
-        email: 'JohnJohni_papa@gmail.com'
-    })
-    res.send(await User.find())
-})
-router.get(config.get_user, async (req, res) => {
-    res.send(await User.find())
-})
+router.post("/api/v1/users", async (req, res) => {
+  await User.create({
+    firstName: "John",
+    lastName: "doeeee",
+    email: "JohnJohni_papa@gmail.com"
+  });
+  res.send(await User.find());
+});
+
+router.get("/api/v1/users", async (req, res) => {
+  res.send(await User.find());
+});
 
 module.exports = router;
